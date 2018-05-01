043b877a7f699c3eaa9f3a3192dc2b42ef2b8409
Jarrod Luca <jarrodluca@gmail.com>
2018-04-30 22:22:50 -0400

    final menu animation

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ $(()=> {
       $('.a').toggleClass('one-full-height');
       $('.b').toggleClass('two-full-width');
       $('.btn').toggleClass('active');
+      $('.btn>span').toggleClass('active-color');
       $('.menu').toggleClass('open');
+      $('.btn span')[0].innerHTML = 'menu';
       setTimeout(()=> {
         $('.a').toggleClass('one-full-width');
         $('.b').toggleClass('two-full-height');
@@ -14,7 +16,9 @@ $(()=> {
       $('.a').toggleClass('one-full-width');
       $('.b').toggleClass('two-full-height');
       $('.btn').toggleClass('active');
+      $('.btn>span').toggleClass('active-color');
       $('.menu').toggleClass('open');
+      $('.btn span')[0].innerHTML = 'close';
       setTimeout(()=> {
         $('.a').toggleClass('one-full-height');
         $('.b').toggleClass('two-full-width');
