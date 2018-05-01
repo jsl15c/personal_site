b1d04b62b20264f8f965bcf1fe9c6b179321c5f6
Jarrod Luca <jarrodluca@gmail.com>
2018-05-01 00:18:12 -0400

    push to test on mobile

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(()=> {
       $('.btn>span').toggleClass('active-color');
       $('.menu').toggleClass('open');
       $('.btn span')[0].innerHTML = 'menu';
+      $('.slide-info').removeClass('slide-active');
+      $('.slide-info div').removeClass('block');
       setTimeout(()=> {
         $('.a').toggleClass('one-full-width');
         $('.b').toggleClass('two-full-height');
@@ -24,5 +26,25 @@ $(()=> {
         $('.b').toggleClass('two-full-width');
       }, 250);
     }
+  });
+
+  $('.about').click(()=> {
+    $('.slide-info').addClass('slide-active');
+    $('.slide-info .about-info').addClass('block');
+  });
+
+  $('.projects').click(()=> {
+    $('.slide-info').addClass('slide-active');
+    $('.slide-info .projects-info').addClass('block');
+  });
+
+  $('.skills').click(()=> {
+    $('.slide-info').addClass('slide-active');
+    $('.slide-info .skill-info').addClass('block');
+  });
+
+  $('.close-info').click(()=> {
+    $('.slide-info').removeClass('slide-active');
+    $('.slide-info div').removeClass('block');
   });
 });
